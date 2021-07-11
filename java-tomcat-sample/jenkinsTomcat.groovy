aec1e40cdc68e074138649b036e78e0eb2238e31
sanu peter <sanupeter25@gmail.com>
2021-07-12 00:51:22 +0530

    dfdfksd

--- java-tomcat-sample/jenkinsTomcat.groovy
+++ java-tomcat-sample/jenkinsTomcat.groovy
@@ -1,5 +1,9 @@
 job('sun-Jenkins-tomcat-package-job-createdby-DSL'){
+    description('This job will generate a war file.')
     logRotator(5,5)
+    wrappers {
+        timestamps()
+    }
     scm{
         git('https://github.com/petersanu54/jenkins-tomcat-repo.git','main')
     }
@@ -15,9 +19,18 @@ job('sun-Jenkins-tomcat-package-job-createdby-DSL'){
     }
 }
 
-job('sun-Jenkins-tomcat-deploy-to-stage'){
+job('sun-Jenkins-tomcat-deploy-to-stage') {
+    description('This job will copy the artificats from upstream and deploy in staging environment.')
     logRotator(5,5)
-    triggers{
-        scm('* * * * *')
+    wrappers {
+        timestamps()
+    }
+    steps{
+        copyArtifcats('upstream'){
+            includePattern('**/*.war')
+            buildSelector{
+                latestSuccessful(true)
+            }
+        }
     }
 }
