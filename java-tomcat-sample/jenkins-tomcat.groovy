077f78eda68d05ac7366314d3857c643be456f80
sanu peter <sanupeter25@gmail.com>
2021-07-11 21:46:55 +0530

    Sknglksl

---
+++ java-tomcat-sample/jenkins-tomcat.groovy
@@ -0,0 +1,15 @@
+job('Jenkins-tomcat-package-job-createdby-DSL'){
+    logRotator(5,5)
+    scm{
+        git('https://github.com/petersanu54/jenkins-tomcat-repo.git','master')
+    }
+    triggers{
+        scm(* * * * *)
+    }
+    steps{
+        mvn('clean package', 'java-tomcat-sample/pom.xml')
+    }
+    publishers{
+        archiveArtifacts('**/*.war')
+    }
+}
